65f9b21093c4b74128f882701799395f4b505c0d
aneeqh <husainaneeq99@gmail.com>
2022-04-20 00:38:56 -0400

    Produced summary table for norm counts

--- Tab2_app.R
+++ Tab2_app.R
@@ -24,6 +24,7 @@ ui <- fluidPage(
   ))
 
 server <- function(input, output, session){
+  options(shiny.maxRequestSize=30*1024^2)
   #function to take norm counts input file
   load_counts <- reactive({
     if (!is.null(input$countsFP)){
@@ -33,7 +34,36 @@ server <- function(input, output, session){
       return(NULL)
     }
   })
+  #function to produce summary table
+  counts_summ <- function(counts_tib, perc_var, nz_genes){
+    if (!is.null(input$countsFP)){
+      print('Here!')
+      tot_samp <- ncol(counts_tib)-1  #store original number of samples and genes
+      tot_genes <- nrow(counts_tib)
+      counts_tib <- counts %>% mutate(variance = apply(counts[-1], MARGIN = 1, FUN = var))  #calculate variance and then percentile
+      perc_val <- quantile(counts_tib$variance, probs = perc_var/100)   #calculate percentile
+      counts_tib <- filter(counts_tib, variance >= perc_val)  #filter by percentile
+      print('here2')
+      counts_tib <- na_if(counts_tib, 0)    #make zeroes NA's
+      counts_tib$non_zero <- tot_samp-rowSums(is.na(counts_tib))  #calculate no. of genes with enough non zero samples
+      counts_tib <- filter(counts_tib, non_zero >= nz_genes)  #filter by non-zero samples
+      print('here3')
+      filt_genes <- nrow(counts_tib)    #calculate the number and % of genes passing the filters
+      perc_pass_genes <- filt_genes/tot_genes*100
+      fail_genes <- tot_genes-filt_genes
+      perc_fail_genes <- fail_genes/tot_genes*100
+      #produce the summary tibble
+      print('here4')
+      summ_tib <- tibble('Measure' = c('Number of Samples', 'Number of Rows', 'No. of genes passing filters', "Percentage (%) passing filter", 'No. of genes not passing filters', 'Percentafe (%) not passing filter'),
+                         'Value' = c(tot_samp, tot_genes, filt_genes, perc_pass_genes, fail_genes, perc_fail_genes))
+      return(summ_tib)}
+    else{return(NULL)}
+  }
   
+  #methods to render tables and plots
+  output$normsumm <- renderTable({
+    counts_summ(load_counts(), input$percvar, input$nonzero)
+  })
 }
 
 shinyApp(ui=ui, server = server)
--- test.R
+++ test.R
@@ -29,22 +29,24 @@ plot_aod <- function(meta_tib){
 #normalize the counts data
 counts <- read_csv(norm_counts_path)
 
-counts_summ <- function(counts_tib, perc_var, ngenes){
-  #original number of samples and genes-
-  nsamp <- ncol(counts_tib)-1
-  ngenes <- nrows(counts_tib)
-  #calculate variance and then percentile-
-  counts_tib <- counts_tib %>% rowwise() %>%
-    mutate(variance = var(c_across(2:69)))
-  #calculate percentile -
-  perc_val <- quantile(counts_tib$variance, probs = perc_var/100)
-  #filter by percentile-
-  counts_tib <- filter(counts_tib, variance >= perc_val)
-  #calculate no. of genes with enough non zero samples
-  
-  #then produce the summary tibble
-  summ_tib <- tibble('Measure' = c('Number of Samples', 'Number of Rows') , 'Value' = c(nsamp, ngenes))
-    
+counts_summ <- function(counts_tib, perc_var, nz_genes){
+  tot_samp <- ncol(counts_tib)-1  #store original number of samples and genes
+  tot_genes <- nrow(counts_tib)
+  counts_tib <- counts %>% mutate(variance = apply(counts[-1], MARGIN = 1, FUN = var))  #calculate variance and then percentile
+  perc_val <- quantile(counts_tib$variance, probs = perc_var/100)   #calculate percentile
+  counts_tib <- filter(counts_tib, variance >= perc_val)  #filter by percentile
+  counts_tib <- na_if(counts_tib, 0)    #make zeroes NA's
+  counts_tib$non_zero <- tot_samp-rowSums(is.na(counts_tib))  #calculate no. of genes with enough non zero samples
+  counts_tib <- filter(counts_tib, non_zero >= nz_genes)  #filter by non-zero samples
+  filt_genes <- nrow(counts_tib)    #calculate the number and % of genes passing the filters
+  perc_pass_genes <- filt_genes/tot_genes*100
+  fail_genes <- tot_genes-filt_genes
+  perc_fail_genes <- fail_genes/tot_genes*100
+  #produce the summary tibble
+  summ_tib <- tibble('Measure' = c('Number of Samples', 'Number of Rows', 'No. of genes passing filters', "Percentage (%) passing filter", 'No. of genes not passing filters', 'Percentafe (%) not passing filter'),
+                     'Value' = c(tot_samp, tot_genes, filt_genes, perc_pass_genes, fail_genes, perc_fail_genes))
   return(summ_tib)
 }
-counts_summ(counts)
+counts_summ(counts, 10, 65)
+
+
